c7d6941df32339330b76571c209662a2a1dfd0ec
Shoval <shovi22@gmail.com>
2020-09-02 23:47:34 +0300

    final version

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Command(models.Model):
     name = models.CharField(max_length=40, blank=False)
-    path = models.CharField(max_length=40, blank=False)
+    # path = models.CharField(max_length=40, blank=False)
 
     def __str__(self):
         return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -6,19 +6,37 @@ from django.views.generic.edit import FormView
 from django.urls import reverse
 import os.path
 from main_app import commands
+from django.views.generic.list import ListView
+from django.utils import timezone
+
 # Create your views here.
+
+class CommandListView(ListView):
+
+    model = Command
+    paginate_by = 100  # if pagination is desired
+    template_name = 'home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
 def do_action_view(request, action):
     try:
         command = Command.objects.get(name=action)
-        if command:
-            file_name = f"commands.{command.path}"
-            module = __import__(file_name)
-            func = getattr(module, command.name)
-            func()
+        activate_action(command)
+        return HttpResponseRedirect(reverse('main_app:home'))
+
     except:
-    # else:
+        # else:
         # upload_file(request)
         return HttpResponseRedirect(reverse('main_app:upload'))
+
+def activate_action(command):
+    # TODO run function from 'commands' dir
+
+    func = getattr(commands, command.name)
+    func()
 
 
 def upload_file_view(request):
@@ -26,42 +44,41 @@ def upload_file_view(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             add_action(request.FILES['file'], request.POST['title'])
-            return HttpResponseRedirect('/success/url/')
+            return HttpResponseRedirect(reverse('main_app:home'))
+
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
 
+
 def add_action(file, name):
     # target = open("name.txt", "a+")
-    save_path = 'C:/Users/shovi/PycharmProjects/AssistantProject/main_app/commands/'
-
-    name_of_file = name
-
-    completeName = os.path.join(save_path, name_of_file + ".py")
-
-    # file1 = open(completeName, "w")
+    save_path = 'main_app/commands/'
+    completeName = os.path.join(save_path, name + ".py")
 
     with open(completeName, 'wb+') as destination:
         for chunk in file.chunks():
             destination.write(chunk)
-        # print(destination)
 
-    # file1.close()
+    with open('main_app/commands/__init__.py','a') as imports:
+        imports.write(f'\nfrom .{name} import {name}')
+
+    c = Command(name=name)
+    c.save()
+    # TODO check add to DB
 
 
-class UploadView(FormView):
-    form_class = UploadFileForm
-    template_name = 'upload.html'
-    success_url = '/thanks/'
-    # success_url = '/thanks/'
-    # def post(self, request, *args, **kwargs):
-    #     form = UploadFileForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         # add_action(request.FILES['file'])
-    #         return HttpResponseRedirect('/success/url/')
 
-    def form_valid(self, form):
-        # add_action(request.FILES['file'])
-        print('valid')
-        # return HttpResponseRedirect('/success/url/')
-        return super().form_valid(form)
+
+
+
+# class UploadView(FormView):
+#     form_class = UploadFileForm
+#     template_name = 'upload.html'
+#     success_url = '/thanks/'
+#
+#     def form_valid(self, form):
+#         # add_action(request.FILES['file'])
+#         print('valid')
+#         # return HttpResponseRedirect('/success/url/')
+#         return super().form_valid(form)
